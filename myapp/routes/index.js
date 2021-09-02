b7e44104a154cc64978b80bb65f2d962dbf902b0
Santiago Matheu <69361600+Santiagomatheu@users.noreply.github.com>
2021-09-02 16:04:05 -0400

    Primeros cambios de rutas a views
    Cambios de rutas

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -6,4 +6,39 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+//Get home page en todos lados
+router.get('/index.ejs', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+//Get detalleUsuario
+router.get('/detalleUsuario.ejs', function(req, res, next) {
+  res.render('detalleUsuario', { title: 'Express' });
+});
+
+//Get detallePost
+router.get('/detallePost.ejs', function(req, res, next) {
+  res.render('detallePost', { title: 'Express' });
+});
+
+//Get login
+router.get('/login.ejs', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
+
+//Get registracion
+router.get('/registracion.ejs', function(req, res, next) {
+  res.render('registracion', { title: 'Express' });
+});
+
+//Get agregarPost
+router.get('/agregarPost.ejs', function(req, res, next) {
+  res.render('agregarPost', { title: 'Express' });
+});
+
+//Get Resultado Busqueda NO ENCUENTRO HTML DE RESULTADOBUSQUEDA
+router.get('/resultadoBusqueda.ejs', function(req, res, next) {
+  res.render('resultadoBusqueda', { title: 'Express' });
+});
+
 module.exports = router;
