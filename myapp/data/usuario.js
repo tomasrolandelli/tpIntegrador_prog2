6f57ddde2b9827b9ed371416d9b51326ea9ce006
TeoSantilli <teosantillipazos@hotmail.com>
2021-09-13 17:48:44 -0300

    Data usuarios

---
+++ myapp/data/usuario.js
@@ -0,0 +1,57 @@
+const usuario={
+    
+    list: [
+        {
+            name:'Lionel Andres',
+            lastName:'Messi',
+            username:'leomessi',
+            biografia:'Bienvenidos a mi cuenta oficial. Actual jugador del PSG y capitan de la Seleccion Argentina.',
+            fotoPerfil:'images/messi.jpeg',
+            idUser:'1',
+
+        },
+        {
+            name:'Cristiano',
+            lastName:'Ronaldo',
+            username:'cristiano',
+            biografia:'Numero 7 del Manchester United y Portugal. Maximo goleador historico, SIUUUUU',
+            fotoPerfil:'images/cristiano.jpg',
+            idUser:'2',
+
+        },
+        {
+            name:'Neymar Jr',
+            lastName:'da Silva Santos',
+            username:'neymarjr',
+            biografia:'10. Brasil y PSG. 99% Fe, 1% Posibilidades.',
+            fotoPerfil:'images/neymar.jpg',
+            idUser:'3',
+
+        },
+        {
+            name:'Robert',
+            lastName:'Lewandowski',
+            username:'_rl9',
+            biografia:'Jugador del Bayern Munich y del seleccionado de Polonia. Numero 9, LEWANGOALSKI',
+            fotoPerfil:'images/lewan.jpg',
+            idUser:'4',
+
+        },
+        {
+            name:'Kylian',
+            lastName:'Mbappe',
+            username:'k.mbappe',
+            biografia:'PSG & France. Campeon Mundial 2018.',
+            fotoPerfil:'images/mbappe.jpg',
+            idUser:'5',
+
+        },
+
+
+
+    ]
+
+
+};
+
+module.exports = usuario;
