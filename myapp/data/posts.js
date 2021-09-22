6520463bb9f39fa0b71120d80c0d9649174b9e46
TeoSantilli <teosantillipazos@hotmail.com>
2021-09-22 17:38:37 -0300

    agrego posteos

---
+++ myapp/data/posts.js
@@ -0,0 +1,55 @@
+const posts = {
+    list: [
+        {
+            username:'leomessi',
+            pie:'Se viene...',
+            imagen:'images/messiPost.jpg',
+            postId:'1',
+            comment1:'Ganas de verte jugar sobran',
+            comment2:'Vamos Leo!!!',
+            comment3:'Grande',
+            comment4:'Extraterrestre',
+        },
+        {
+            username:'cristiano',
+            pie:'Same place... Same spirit... New adventure... Lets go, Devils!',
+            imagen:'images/cristianoPost.jpg',
+            postId:'2',
+            comment1:'Good luck!',
+            comment2:'Lets gooo!',
+            comment3:'This is magic',
+            comment4:'Welcome back',
+        },
+        {
+            username:'neymarjr',
+            pie:'3 pontos importantes. ALLEZ PSG',
+            imagen:'images/neymarPost.jpg',
+            postId:'3',
+            comment1:'Gloria',
+            comment2:'Amen',
+            comment3:'Maestro',
+            comment4:'Vamoss!',
+        },
+        {
+            username:'_rl9',
+            pie:'Today you can call me Robert LewanGOLDski',
+            imagen:'images/lewanPost.jpg',
+            postId:'4',
+            comment1:'LewanGOALski',
+            comment2:'Maquina',
+            comment3:'Well deserved!!',
+            comment4:'Congrats',
+        },
+        {
+            username:'k.mbappe',
+            pie:'Grande victoire à la maison',
+            imagen:'images/mbappePost.jpg',
+            postId:'5',
+            comment1:'Future GOAT',
+            comment2:'Come to Madrid',
+            comment3:'Bravoo',
+            comment4:'Icon',
+        },
+
+    ]
+}
