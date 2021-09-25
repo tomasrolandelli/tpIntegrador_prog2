001f7ce01149da9e927cb5803cecf0cd7260e19d
tomasrolandelli <tomyrolandelli@gmail.com>
2021-09-25 14:31:59 -0300

    cambios chicos

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -4,14 +4,9 @@ var router = express.Router();
 
 //VARIABLES
 let indexController = require('../controllers/indexController')
-let detallePostController = require('../controllers/detallePostController')
-let detalleUsuarioController = require('../controllers/detalleUsuarioController')
-
 
 //RUTA
 router.get('/', indexController.index);
-router.get('/detalle-post', detallePostController.index);
-router.get('/detalle-usuario', detalleUsuarioController.index);
 
 
 //EXPORTAR
